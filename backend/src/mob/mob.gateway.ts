d392c3ac1ee509324eb3cdcabdcd32307f46fa7e
Patalahin <spatalaxin@bk.ru>
2024-08-03 19:00:37 +0300

    add cors to gateways

--- backend/src/mob/mob.gateway.ts
+++ backend/src/mob/mob.gateway.ts
@@ -1,7 +1,13 @@
 import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+})
 export class MobGateway {
   @WebSocketServer()
   server: Server;
