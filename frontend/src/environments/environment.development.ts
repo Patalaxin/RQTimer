a5b35e6f183676fe0dfe69e50ccaade225fbbcb4
Patalahin <spatalaxin@bk.ru>
2024-12-10 18:02:50 +0300

    Change api urls

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -2,6 +2,5 @@ export const environment = {
   production: true,
   url: 'https://www.rqtimer.ru/',
   apiUrl: 'https://www.rqtimer.ru/api',
-  // apiUrl: 'http://localhost:3000',
-  timezoneDb: '4WO2T6BMUPV3',
+  // apiUrl: 'http://localhost:3000'
 };
