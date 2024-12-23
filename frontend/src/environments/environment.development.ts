5ce91b846149fe799942dd368ba311c95444865c
Patalahin <spatalaxin@bk.ru>
2024-12-23 22:35:40 +0300

    - Add Astus, Aztec, Ortos ans Pyros servers on backend schemas

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url: 'https://www.rqtimer.ru/',
-  apiUrl: 'https://www.rqtimer.ru/api',
-  // apiUrl: 'http://localhost:3000'
+  // apiUrl: 'https://www.rqtimer.ru/api',
+  apiUrl: 'http://localhost:3000',
 };
