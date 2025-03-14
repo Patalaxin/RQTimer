edd842dab29510f54988ad5e0c1e3427ec5c5002
Patalahin <spatalaxin@bk.ru>
2025-03-15 01:36:17 +0300

    - init tg bot

--- backend/src/bot/telegram-bot.module.ts
+++ backend/src/bot/telegram-bot.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BotSession, BotSessionSchema } from '../schemas/telegram-bot.schema';
 import { User, UserSchema } from '../schemas/user.schema';
+import { Mob, MobSchema } from '../schemas/mob.schema'; // Импорт схемы для Mob
 import { TelegramBotService } from './telegram-bot.service';
+import { MobModule } from '../mob/mob.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: BotSession.name, schema: BotSessionSchema },
       { name: User.name, schema: UserSchema },
+      { name: Mob.name, schema: MobSchema },
     ]),
+    MobModule,
   ],
   providers: [TelegramBotService],
   exports: [TelegramBotService],
