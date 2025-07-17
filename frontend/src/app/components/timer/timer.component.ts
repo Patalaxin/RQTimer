b71cdb7df30502378be11b9dd89383fa6830c48f
zloy_monk <phamnhuquockhanh@yandex.ru>
2025-07-17 14:25:37 +0700

    fix prod url

--- frontend/src/app/components/timer/timer.component.ts
+++ frontend/src/app/components/timer/timer.component.ts
@@ -530,13 +530,13 @@ export class TimerComponent implements OnInit, OnDestroy {
 
       if (specialNotification && timeDifference === 1) {
         const audio = new Audio(
-          `${this.IMAGE_SRC}/1mDeadVoices/${item.mobData.mobId}.m4a`,
+          `${this.IMAGE_SRC}1mDeadVoices/${item.mobData.mobId}.m4a`,
         );
         audio.volume = volume;
         enqueueAudio(audio);
       } else if (specialNotification && timeDifference === 0) {
         const audio = new Audio(
-          `${this.IMAGE_SRC}/deadVoices/${item.mobData.mobId}.m4a`,
+          `${this.IMAGE_SRC}deadVoices/${item.mobData.mobId}.m4a`,
         );
         audio.volume = volume;
         enqueueAudio(audio);
@@ -554,7 +554,7 @@ export class TimerComponent implements OnInit, OnDestroy {
     ) => {
       new Notification(title, {
         body,
-        icon: `${this.IMAGE_SRC}/${item.mob.image}`,
+        icon: `${this.IMAGE_SRC}${item.mob.image}`,
       });
       playSound(timeDifference);
     };
